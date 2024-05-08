2f9f1fb7752117947e8fff68959ca54cedf5d3c4
NirmitBhatt <nirmitb97@gmail.com>
2024-05-08 20:00:47 +0200

    Added Categories Controller and related Index.cshtml View file

---
+++ Supermarket_Management_System/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Supermarket_Management_System.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id.HasValue)
+            {
+                return new ContentResult { Content = "The id of this product is: " + id.ToString() };
+            }
+            else
+            {
+                return new ContentResult { Content = "Null content" };
+            }
+            
+        }
+    }
+}
