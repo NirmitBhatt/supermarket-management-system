f888938732bf834480b7365ae737fc986c0e7af8
NirmitBhatt <nirmitb97@gmail.com>
2024-06-30 23:41:00 +0200

    Clean Architecture: Added ViewCategories Use Case

--- UseCases/CategoriesUseCases/ViewCategoriesUseCase.cs
+++ UseCases/CategoriesUseCases/ViewCategoriesUseCase.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using CoreBusinessEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
 
 namespace UseCases.CategoriesUseCases
 {
-    internal class ViewCategoriesUseCase
+    public class ViewCategoriesUseCase
     {
+        private readonly ICategoryRepository categoryRepository;
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository) 
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public IEnumerable<Category> Execute()
+        {
+            return categoryRepository.GetCategories();
+        }
     }
 }
